5ee8fe19f662d0e98d4806bbde6b039a45fbe35a
C3viche <28328104+C3viche@users.noreply.github.com>
2025-01-28 15:10:01 -0800

    added some linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+    ...compat.config({
+        extends: ['next/core-web-vitals', 'prettier'],
+    }),
+    {
+        rules: {
+            "prettier/prettier": [
+                "error",
+                {
+                    "bracketSpacing": true,
+                    "tabWidth": 2,
+                    "printWidth": 80,
+                    "useTabs": false,
+                    "trailingComma": "es5",
+                    "endOfLine": "auto",
+                    "semi": true
+                }
+            ],
+            "no-unused-vars": [
+                "error",
+                { 
+                  "vars": "all", 
+                  "varsIgnorePattern": "^_*$",
+                  "argsIgnorePattern": "^_*$",
+                  "destructuredArrayIgnorePattern": "^_*$"
+                }
+            ],
+            "eqeqeq": [
+                "error",
+                "always"
+            ],
+        }
+    }
+];
 
 export default eslintConfig;
