a92d285a9ef59387771b4049848a74b3d57b6853
michelleyeoh <michellew.yeoh@gmail.com>
2025-01-28 16:07:15 -0800

    globals

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -1,5 +1,6 @@
 import Header from "./_components/Header/Header";
 import Footer from "./_components/Footer/Footer";
+import './_globals/globals.scss';
 
 export const metadata = {
   title: "Create Next App",
